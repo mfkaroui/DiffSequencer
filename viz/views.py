b3bae4cc7715a5d29df16e7bc411f5e62ec0a061
Mohamed Karoui <mfkaroui@gmail.com>
2019-07-21 02:56:27 -0400

    added backend view

--- viz/views.py
+++ viz/views.py
@@ -14,8 +14,24 @@ class MainView(flaskc.FlaskView):
     def dashboard(self):
         return flask.render_template("dashboard/index.html")
 
+class BackendView(flaskc.FlaskView):
+    route_base = "/backend/"
+
+    def __init__(self, sharedVars):
+        self.sharedVars = sharedVars
+
+    @flaskc.route("/hardwareStats", methods=["GET"])
+    def hardwareStats(self):
+        return flask.jsonify(self.sharedVars.getHardwareStats())
+
+    @flaskc.route("/sequences/list", methods=["GET"])
+    def sequences_list(self):
+        pass
+    
+
 def initializeViews(workingDIR):
     app = flask.Flask("Viz", template_folder=os.path.join(workingDIR, "viz/templates"), static_folder=os.path.join(workingDIR, "viz/static"))
     variables = shared.Shared(app, os.path.join(workingDIR, "data/config.json"))
     MainView.register(app, init_argument=variables)
+    BackendView.register(app, init_argument=variables)
     app.run(host="0.0.0.0", port="1337", debug=True)
